32c760330ddf3052e970c42e3963701958e574cd
Steve Hallam <StevenAHallam@gmail.com>
2018-03-04 09:37:29 -0800

    Basic proxy server implementation with photo-stream module.

--- proxy/server.js
+++ proxy/server.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const morgan = require('morgan');
 const path = require('path');
+const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/photos', (req, res) => {
+  let id = req.query;
+  axios.get('http://localhost:3001/photos', {
+      params: id
+    }).
+      then((data) => {
+        let photos = data.data;
+        res.send(photos);
+      }).
+      catch((error) => {
+        console.log(error);
+      });
+})
+
 app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`)
+  console.log(`server running at: http://localhost:${port}`);
 });
